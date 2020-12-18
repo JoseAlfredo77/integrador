7581ae9688ccddd7a48c93eb08f3da052e0a04da
JoseAlfredo77 <48421518+JoseAlfredo77@users.noreply.github.com>
2020-12-18 09:17:14 -0600

    Update Codigo1.ino

--- Codigo1.ino
+++ Codigo1.ino
@@ -2,18 +2,26 @@ int P1P = A1;
 int P2P = A2;
 int P3P = A3;
 
-const int 
+const int A=10,B=11,C=12,D=13, S=9;
+float v, dis=1; //Dis -  Distancia entre sensores (cm);
+
 float P1, P2, P3;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  for(int x=10;x<=13;x++){
+    pinMode(x,OUTPUT);
+  }
+  for(int x=9;x<=9;x++){
+    pinMode(x,INPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   adquisicion();
-  
-  
+  sensado_vaso();
+  Serial.println(v);
 }
 void adquisicion(){
   P1 = analogRead(P1P);
@@ -25,3 +33,26 @@ void adquisicion(){
   P3 = map(P3,0,1023,0,100);
   
 }
+
+int sensado_vaso(){
+  v=1;
+  int muxValue,a=-1;
+  for (byte i = 0; i < 15; i++)
+   {
+      SetMuxChannel(i);
+      muxValue = digitalRead(S);
+      delay(50);
+      if (muxValue == 0 && a==-1)a=v;
+      v++;
+   }
+   v=a*dis;
+   delay(1000);
+}
+
+int SetMuxChannel(byte channel){
+   digitalWrite(A, bitRead(channel, 0));
+   digitalWrite(B, bitRead(channel, 1));
+   digitalWrite(C, bitRead(channel, 2));
+   digitalWrite(D, bitRead(channel, 3));
+}
+ 
